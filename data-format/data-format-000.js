82268eeda7722868fc23d6c2671b9739bcd1df03
Simon Boisclair <simonboisclair553@hotmail.com>
2019-10-16 13:16:07 -0400

    change_data format

---
+++ data-format/data-format-000.js
@@ -0,0 +1,31 @@
+let dataFormat000 = {
+  weedDetectionCamera : {
+    camera1 : {
+      timeStamp : {
+        heightArr : {
+          widthArr : {
+            1 : {
+              r : "Number",
+              g : "Number",
+              b : "Number",
+            },
+            // ...widthImage
+          }
+          // ...heightImage
+        }
+        // ...framesSent
+      }
+    },
+    cameraOffset : {
+      x : "Number",
+      y : "Number",
+      z : "Number",
+    }
+    // ...cameraNbr
+  },
+  
+  orientationCamera : {},
+  location : {},
+  angle : {},
+  limits : {},
+}
